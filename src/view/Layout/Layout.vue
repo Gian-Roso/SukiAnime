ce3df1610682ac90a12181abc366132a6347c1ed
Gian-Roso <gianoficial305@gmail.com>
2025-04-07 13:11:22 -0300

    Fix: concerto da posição da tela e adição de sombra

--- src/view/Layout/Layout.vue
+++ src/view/Layout/Layout.vue
@@ -1,7 +1,8 @@
 <template>
   <v-app-bar  
     height="180"
-    class="ColorBack overflow-visible pb-4"
+    class="overflow-visible pb-4"
+    :style="{ background: 'linear-gradient(to right, #C2E9FB 0%, #F1B7EF 20%, #F1B7EF 50%, #F1B7EF 80%, #C2E9FB 100%)', color: 'white' }"
   >
     <v-container fluid>
       <v-row no-gutters class="justify-center">
@@ -9,7 +10,7 @@
         <v-col cols="12" class="d-flex justify-center align-center position-relative mb-4">
           <h1
             class="text-white text-h1 ma-0 z-index-1 font-italic font-weight-bold"
-            style="position: relative; text-shadow: 2px 2px 4px rgba(0,0,0,0.5);"
+            style="position: relative; text-shadow: 2px 2px 4px rgba(0,0,0,0.5); translate: -5%"
           >
             Suki🎔Anime
           </h1>
@@ -19,26 +20,27 @@
             height="140"
             width="140"
             class="position-absolute z-index-2"
-            :style="{ left: 'calc(50% + 260px)', top: '50%', transform: 'translateY(-50%)' }"
+            :style="{ left: 'calc(48% + 260px)', top: '50%', transform: 'translateY(-50%)' }"
             cover
           />
           <v-sheet
             class="position-absolute"
             :style="{
               top: 'calc(16.5% + 80px)',
-              left: '50%',
+              left: '47%',
               transform: 'translateX(-41%)',
               width: '680px',
               height: '6px',
               backgroundColor: 'white',
-              borderRadius: '3px'
+              borderRadius: '3px',
+              boxShadow: '0 2px 6px rgba(0,0,0,0.3)'
             }"
           />
         </v-col>
         <!-- Botões abaixo -->
         <v-col cols="12" class="d-flex justify-center flex-wrap"
         :style="{
-              transform: 'translateX(4%)'
+              transform: 'translateX(1.5%)'
             }">
           <v-btn
             v-for="btn in ['Home', 'Calendary', 'List', 'News', 'About']"
@@ -47,9 +49,9 @@
             height="50"
             class="mx-3 my-2"
             variant="outlined"
-            style="border-width: 3px;"
+            style="border-width: 3px; box-shadow: 2px 2px 4px rgba(0,0,0,0.5)"
           >
-
+            
             {{ btn }}
           </v-btn>
         </v-col>
