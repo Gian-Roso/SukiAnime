34d6edcc72a9980ec3bfd39ccd722cc4472c095c
Gian <gianoficial305@gmail.com>
2025-04-07 20:27:53 -0300

    ADD: aleterão e adição das rotas dos botões

--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/view/pages/Home.vue' // ajusta se teu path for outro
+import CalendaryPage from '@/view/pages/CalendaryPage.vue'
+import About from '@/view/pages/About.vue'
+import News from '@/view/pages/News.vue'
+import List from '@/view/pages/List.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/Calendary',
+    name: 'Calendary',
+    component: CalendaryPage
+  },
+  {
+    path: '/List',
+    name: 'List Anime',
+    component: List
+  },
+  {
+    path: '/News',
+    name: 'News',
+    component: About
+  },
+  {
+    path: '/About',
+    name: 'About us',
+    component: News
   }
 ]
 
